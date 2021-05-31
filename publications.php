6e5eeefebb829e180f9bd8161fc516e8fcd7680c
DeliDro <deli-dro@live.fr>
2021-05-31 09:12:00 +0000

    2021/05/31 09:11 - Affichage Produits + Publications

--- publications.php
+++ publications.php
@@ -1,4 +1,10 @@
 <?php session_start() ?>
+<?php
+    $publications = json_decode(
+        file_get_contents("admin/data-handler/data/publications.json"),
+        JSON_HEX_TAG
+    );
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -50,181 +56,61 @@
     <!--page start-->
     <div class="page">
 
-        <!-- preloader start -->
-        <div id="preloader">
-          <div id="status">&nbsp;</div>
-        </div>
-        <!-- preloader end -->
-
-        <!--header start-->
-        <header id="masthead" class="header ttm-header-style-classic">
-            <!-- ttm-topbar-wrapper -->
-            <div class="ttm-topbar-wrapper ttm-bgcolor-darkgrey ttm-textcolor-white clearfix">
-                <div class="container">
-                    <div class="ttm-topbar-content">
-                        <ul class="top-contact ttm-highlight-left text-left">
-                            <li><i class="fa fa-clock-o"></i><strong>Heures d'ouvertures :</strong> 7j/7 8:00 - 18.00</li>
-                        </ul>
-                        
-                        <div class="topbar-right text-right">
-                            <ul class="top-contact">
-                                <li><i class="fa fa-envelope-o"></i><a href="mailto:info@auxano.com">info@auxano.com</a></li>
-                                <li><i class="fa fa-phone"></i>+212 00 000 000</li>
-                            </ul>
-                            
-                            <div class="ttm-social-links-wrapper list-inline">
-                                <ul class="social-icons">
-                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div><!-- ttm-topbar-wrapper end -->
-            
-            <!-- ttm-header-wrap -->
-            <div class="ttm-header-wrap">
-                <!-- ttm-stickable-header-w -->
-                <div id="ttm-stickable-header-w" class="ttm-stickable-header-w clearfix">
-                    <div id="site-header-menu" class="site-header-menu">
-                        <div class="site-header-menu-inner ttm-stickable-header">
-                            <div class="container">
-                                <!-- site-branding -->
-                                <div class="site-branding">
-                                    <a class="home-link" href="index.php" title="Auxano" rel="home">
-                                        <img id="logo-img" class="img-center" src="images/logo.png" alt="logo-img">
-                                    </a>
-                                </div><!-- site-branding end -->
-                                
-                                <!--site-navigation -->
-                                <div id="site-navigation" class="site-navigation">
-                                    
-                                    <div class="ttm-menu-toggle">
-                                        <input type="checkbox" id="menu-toggle-form" />
-                                        <label for="menu-toggle-form" class="ttm-menu-toggle-block">
-                                            <span class="toggle-block toggle-blocks-1"></span>
-                                            <span class="toggle-block toggle-blocks-2"></span>
-                                            <span class="toggle-block toggle-blocks-3"></span>
-                                        </label>
-                                    </div>
-                                    
-                                    <nav id="menu" class="menu">
-                                        <ul class="dropdown">
-                                            <li class="active"><a href="index.php">Accueil</a></li>
-                                            
-                                            <li><a href="#">Activités</a>
-                                                <ul>
-                                                    <li><a href="produits.php">Produits</a></li>
-                                                    <li><a href="services-auxano.php">Services</a></li>
-                                                </ul>
-                                            </li>
-                                            
-                                            <li><a href="publications.php">Publications</a></li>
-                                            
-                                            <li><a href="">A Propos</a></li>
-                                           
-                                            <li><a href="nous-contacter.php">Nous contacter</a></li>
-                                        </ul>
-                                    </nav>
-                                </div><!-- site-navigation end-->
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- ttm-stickable-header-w end-->
-            </div><!--ttm-header-wrap end -->
-        
-        </header><!--header end-->
-        
-        <!-- page-title -->
-        <div class="ttm-page-title-row">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12"> 
-                        <div class="title-box ttm-textcolor-white">
-                            <div class="page-title-heading">
-                                <h1 class="title">Publications</h1>
-                            </div><!-- /.page-title-captions -->
-                            <div class="breadcrumb-wrapper">
-                                <span>
-                                    <a title="Homepage" href="index.php"><i class="ti ti-home"></i>&nbsp;&nbsp;Accueil</a>
-                                </span>
-                                <span class="ttm-bread-sep">&nbsp; | &nbsp;</span>
-                                <span style="font-weight: bold; color: white">Publications</span>
-                            </div>  
-                        </div>
-                    </div><!-- /.col-md-12 -->  
-                </div><!-- /.row -->  
-            </div><!-- /.container -->                      
-        </div><!-- page-title end-->
+        <?php include("components/header.php") ?>
 
         <!--site-main start-->
         <div class="site-main">
+            <?php
+                function getTags($tags) {
+                    $tags = explode(",", $tags);
+                    
+                    for ($i=0; $i < count($tags) ; $i++) { 
+                        # code...
+                        $tags[$i] = "<a>". $tags[$i]. "</a>";
+                    };
+                    return implode("", $tags);
+                }
 
-            <!--blog-section-->
-            <section class="ttm-row blog-left-img-section clearfix">
-                <div class="container">
-                    <!-- row -->
-                    <div class="row">
-                        <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
-                            <!-- featured-imagebox-post -->
-                            <div class="row no-gutters featured-imagebox featured-imagebox-post ttm-box-view-left-image box-shadow">
-                                <div class="col-lg-5 col-md-12 col-sm-6 ttm-featured-img-left">
-                                    <div class="featured-thumbnail"> 
-                                        <img class="img-fluid" src="https://via.placeholder.com/450X600/444444.jpg" alt="image"> 
-                                    </div> 
+                $publications = $publications["publications"];
+                $output = "";
+                
+                for ($i=0; $i < count($publications); $i++) { 
+                    # code...
+                    $item = $publications[$i];
+                    $output = '
+                    <section class="ttm-row blog-left-img-section clearfix">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
+                                    <div class="row no-gutters featured-imagebox featured-imagebox-post ttm-box-view-left-image box-shadow">
+                                        <div class="col-lg-5 col-md-12 col-sm-6 ttm-featured-img-left">
+                                            <div class="featured-thumbnail"> 
+                                                <img class="img-fluid" src="'. $item["image"] .'" alt="image"> 
+                                            </div> 
+                                        </div>
+                                        <div class="col-lg-7 col-md-12 col-sm-6 featured-content featured-content-post">
+                                            <span class="category">
+                                                '. getTags($item["tags"]) .'
+                                            </span>
+                                            <div class="post-title featured-title">
+                                                <h5><a href="publication.php?id='. $item["id"] .'">'. $item["titre"] .'</a></h5>
+                                            </div>
+                                            <div class="featured-desc ttm-box-desc">
+                                                <p>'. substr($item["contenu"], 0, 100) .'...</p>
+                                            </div>
+                                            <div class="post-meta">
+                                                <span class="ttm-meta-line"><i class="fa fa-calendar"></i>'. $item["date"] .'</a></span>
+                                                <span class="ttm-meta-line"><i class="fa fa-eye"></i>0 vues</span>
+                                            </div>
+                                        </div>
+                                    </div><!-- featured-imagebox-post end-->
                                 </div>
-                                <div class="col-lg-7 col-md-12 col-sm-6 featured-content featured-content-post">
-                                    <span class="category">
-                                        <a>Tag 1</a>
-                                        <a>Tag 2</a>
-                                    </span>
-                                    <div class="post-title featured-title">
-                                        <h5><a href="publication.php">Titre publication</a></h5>
-                                    </div>
-                                    <div class="featured-desc ttm-box-desc">
-                                        <p>Avant-goût de publication...</p>
-                                    </div>
-                                    <div class="post-meta">
-                                        <span class="ttm-meta-line"><i class="fa fa-calendar"></i>19 Mai 2021</a></span>
-                                        <span class="ttm-meta-line"><i class="fa fa-eye"></i>2 vues</span>
-                                    </div>
-                                </div>
-                            </div><!-- featured-imagebox-post end-->
+                            </div>
                         </div>
-
-                        <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
-                            <!-- featured-imagebox-post -->
-                            <div class="row no-gutters featured-imagebox featured-imagebox-post ttm-box-view-left-image box-shadow">
-                                <div class="col-lg-5 col-md-12 col-sm-6 ttm-featured-img-left">
-                                    <div class="featured-thumbnail"> 
-                                        <img class="img-fluid" src="https://via.placeholder.com/450X600/444444.jpg" alt="image"> 
-                                    </div> 
-                                </div>
-                                <div class="col-lg-7 col-md-12 col-sm-6 featured-content featured-content-post">
-                                    <span class="category">
-                                        <a>Société</a>
-                                        <a>Développement</a>
-                                        <a>RSE</a>
-                                    </span>
-                                    <div class="post-title featured-title">
-                                        <h5><a href="publication.php">Don d'un pont au village de Kanahakiri</a></h5>
-                                    </div>
-                                    <div class="featured-desc ttm-box-desc">
-                                        <p>Dévouvrez en images et en texte ce événement à fort impact social qui crée les conditions de développement de ce petit village du nord de la Côte d'Ivoire...</p>
-                                    </div>
-                                    <div class="post-meta">
-                                        <span class="ttm-meta-line"><i class="fa fa-calendar"></i>19 Mai 2021</a></span>
-                                        <span class="ttm-meta-line"><i class="fa fa-eye"></i>2 vues</span>
-                                    </div>
-                                </div>
-                            </div><!-- featured-imagebox-post end-->
-                        </div>
-                    </div><!-- row end-->
-                </div>
-            </section>
-            <!--blog-section end-->
+                    </section>';
+                }
+                echo $output;
+            ?>
 
         </div><!--site-main end-->
 
@@ -376,7 +262,7 @@
                         <div class="col-md-12">
                             <ul id="menu-footer-menu" class="footer-nav-menu">
                                 <li><a target="_blank" href="">Qui sommes-nous ?</a></li>
-                                <li><a target="_blank" href="produits.php">Produits</a></li>
+                                <li><a target="_blank" href="publications.php">Produits</a></li>
                                 <li><a target="_blank" href="services-auxano.php">Services</a></li>
                             </ul>
                         </div>
